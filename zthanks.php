880a06409ccc0ad37f1323424bac39b06bacb3c0
karimassababi <assababi.k@hotmail.fr>
2022-06-23 14:17:42 +0200

    update ch9 php #2

--- zthanks.php
+++ zthanks.php
@@ -34,13 +34,13 @@ if (empty($_POST['user_tel_number'])) {
 
 if(empty($errors)) {
     echo 'Merci ' . $_POST['user_firstname'] . ' ' . $_POST['user_name'] . ' de nous avoir contactés à propos de ' . $_POST['user_theme_choice'] . '.'; 
-echo BR;
-echo BR;
-echo 'Un de nos conseillers vous contactera soit à l’adresse ' . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['user_tel_number'] . 
-' dans les plus brefs délais pour traiter votre demande. '; 
-echo BR;
-echo BR;
-echo $_POST['user_message'];
+    echo BR;
+    echo BR;
+    echo 'Un de nos conseillers vous contactera soit à l’adresse ' . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['user_tel_number'] . 
+    ' dans les plus brefs délais pour traiter votre demande. '; 
+    echo BR;
+    echo BR;
+    echo $_POST['user_message'];
 }
 
 
